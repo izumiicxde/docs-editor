4414327a51cb619911d19ddc830d96f4cac95de3
izumii <154342465+izumiCode@users.noreply.github.com>
2025-01-08 11:04:42 +0530

    function to delete a document and UI configuration

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Id } from "../../../convex/_generated/dataModel";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLink, MoreVerticalIcon, TrashIcon } from "lucide-react";
+import { RemoveDialog } from "@/components/remove-dialog";
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+}
+export const DocumentMenu = ({
+  documentId,
+  title,
+  onNewTab,
+}: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"ghost"} size={"icon"} className="rounded-full">
+          <MoreVerticalIcon className="size-4 " />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <RemoveDialog documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <TrashIcon className="size-4 mr-2" />
+            Remove
+          </DropdownMenuItem>
+        </RemoveDialog>
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLink className="size-4 mr-2" />
+          Open in new tab
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/app/(home)/document-row.tsx
+++ src/app/(home)/document-row.tsx
@@ -1,15 +1,18 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 import { SiGoogledocs } from "react-icons/si";
 import { Doc } from "../../../convex/_generated/dataModel";
-import { Building2Icon, CircleUserIcon, MoreVertical } from "lucide-react";
+import { Building2Icon, CircleUserIcon } from "lucide-react";
 import { format } from "date-fns";
-import { Button } from "@/components/ui/button";
+import { DocumentMenu } from "./document-menu";
 
 interface DocumentRowProps {
   document: Doc<"documents">;
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+  const onNewTabClick = (id: string) => {
+    window.open(`/documents/${id}`, "_blank");
+  };
   return (
     <TableRow className="cursor-pointer">
       <TableCell className="w-[50px] ">
@@ -28,9 +31,11 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
         {format(new Date(document._creationTime), "MMM dd, yyy")}
       </TableCell>
       <TableCell className="flex justify-end">
-        <Button variant={"ghost"} size={"icon"} className="rounded-full">
-          <MoreVertical className="size-4" />
-        </Button>
+        <DocumentMenu
+          documentId={document._id}
+          title={document.title}
+          onNewTab={onNewTabClick}
+        />
       </TableCell>
     </TableRow>
   );
