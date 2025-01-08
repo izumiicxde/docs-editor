9b56dea23b83444805e02218ab6d040f600863f4
izumii <154342465+izumiCode@users.noreply.github.com>
2025-01-08 19:49:35 +0530

    minor configuration for room loading

--- src/app/documents/[documentId]/Room.tsx
+++ src/app/documents/[documentId]/Room.tsx
@@ -7,13 +7,14 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { FullscreenLoader } from "@/components/fullscreen-loader";
 
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
   return (
     <LiveblocksProvider throttle={16} authEndpoint={"/api/liveblocks-auth"}>
       <RoomProvider id={params.documentId as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense fallback={<FullscreenLoader label="Loading..." />}>
           {children}
         </ClientSideSuspense>
       </RoomProvider>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -5,16 +5,16 @@ import { Room } from "./Room";
 
 export default function page() {
   return (
-    <div className="min-h-screen bg-[#fafbfd] w-full">
-      <div className="w-full flex flex-col px-4 gap-y-2 fixed top-0 left-0 z-10 bg-[#fafbfd] print:hidden">
-        <Navbar />
-        <Toolbar />
-      </div>
-      <div className="pt-[114px] print:pt-0 w-full">
-        <Room>
+    <Room>
+      <div className="min-h-screen bg-[#fafbfd] w-full">
+        <div className="w-full flex flex-col px-4 gap-y-2 fixed top-0 left-0 z-10 bg-[#fafbfd] print:hidden">
+          <Navbar />
+          <Toolbar />
+        </div>
+        <div className="pt-[114px] print:pt-0 w-full">
           <Editor />
-        </Room>
+        </div>
       </div>
-    </div>
+    </Room>
   );
 }
